0b41fbaf13b46b20593eda8983c52b79fbcd99e0
Yves Baumes <ybaumes@gmail.com>
2018-03-19 21:41:04 +0100

    Refactor code into makeInputReader function.

--- coreutils/mcat/mcat.go
+++ coreutils/mcat/mcat.go
@@ -8,32 +8,33 @@ import (
 )
 
 func main() {
-	var inputrdr io.Reader
-	if len(os.Args) == 1 {
-		inputrdr = os.Stdin
-	} else {
-		readers := make([]io.Reader, 0)
-
-		for i, elt := range os.Args {
-			if i == 0 {
-				continue
-			}
-
-			var r io.Reader
-			if f, err := os.Open(elt); err != nil {
-				r = strings.NewReader(err.Error() + "\n")
-			} else {
-				r = f
-			}
-
-			readers = append(readers, r)
-		}
-
-		inputrdr = io.MultiReader(readers...)
-
-	}
-
+	var inputrdr io.Reader = makeInputReader()
 	if _, err := io.Copy(os.Stdout, inputrdr); err != nil {
 		fmt.Println("Err:", err)
 	}
+}
+
+func makeInputReader() io.Reader {
+	if len(os.Args) == 1 {
+		return os.Stdin
+	}
+
+	readers := make([]io.Reader, 0)
+
+	for i, elt := range os.Args {
+		if i == 0 {
+			continue
+		}
+
+		var r io.Reader
+		if f, err := os.Open(elt); err != nil {
+			r = strings.NewReader(err.Error() + "\n")
+		} else {
+			r = f
+		}
+
+		readers = append(readers, r)
+	}
+
+	return io.MultiReader(readers...)
 }
