691323068cb8e139ffe60b8783c879ab54e594ad
Yves Baumes <ybaumes@gmail.com>
2018-03-20 22:57:36 +0100

    Implement showing hidden files.

--- coreutils/mls/mls.go
+++ coreutils/mls/mls.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"strings"
 )
 
 func main() {
+	var showHidden = false
+	flag.BoolVar(&showHidden, "a", false, "Show hidden files.")
+	flag.Parse()
+
 	infos, err := ioutil.ReadDir(".")
 	if err != nil {
 		fmt.Println("Err:", err)
@@ -14,7 +19,7 @@ func main() {
 	}
 
 	for _, fileinfo := range infos {
-		if strings.HasPrefix(fileinfo.Name(), ".") {
+		if strings.HasPrefix(fileinfo.Name(), ".") && !showHidden {
 			continue
 		}
 		fmt.Println(fileinfo.Name())
