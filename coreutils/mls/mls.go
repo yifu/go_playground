9314091d598934a785ca03a1edc30566e8cbda21
Yves Baumes <ybaumes@gmail.com>
2018-03-21 16:56:50 +0100

    Refactor to take file as a param.

--- coreutils/mls/mls.go
+++ coreutils/mls/mls.go
@@ -4,42 +4,62 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 	"syscall"
 )
 
-func main() {
-	var showHidden = false
-	flag.BoolVar(&showHidden, "a", false, "Show hidden files.")
+var showHidden = false
 
-	var showInode = false
+var showInode = false
+
+func main() {
+	flag.BoolVar(&showHidden, "a", false, "Show hidden files.")
 	flag.BoolVar(&showInode, "i", false, "Show inode numbers.")
 
 	flag.Parse()
 
-	var dir string
+	var param string
 	if len(flag.Args()) == 0 {
-		dir = "."
+		param = "."
 	} else {
-		dir = flag.Arg(0)
+		param = flag.Arg(0)
 	}
 
-	infos, err := ioutil.ReadDir(dir)
+	fileInfo, err := os.Stat(param)
+	if err != nil {
+		fmt.Println(os.Args[0]+":", err.Error())
+		return
+	}
+
+	switch {
+	case fileInfo.IsDir():
+		processDir(fileInfo)
+	default:
+		processFile(fileInfo)
+	}
+}
+
+func processDir(dir os.FileInfo) {
+	infos, err := ioutil.ReadDir(dir.Name())
 	if err != nil {
 		fmt.Println("Err:", err)
 		return
 	}
 
-	for _, fileinfo := range infos {
-		if strings.HasPrefix(fileinfo.Name(), ".") && !showHidden {
+	for _, fileInfo := range infos {
+		if strings.HasPrefix(fileInfo.Name(), ".") && !showHidden {
 			continue
 		}
 
-		if showInode {
-			fmt.Print(uint64(fileinfo.Sys().(*syscall.Stat_t).Ino), " ")
-		}
-
-		fmt.Println(fileinfo.Name())
-
+		processFile(fileInfo)
 	}
+}
+
+func processFile(fileInfo os.FileInfo) {
+	if showInode {
+		fmt.Print(uint64(fileInfo.Sys().(*syscall.Stat_t).Ino), " ")
+	}
+
+	fmt.Println(fileInfo.Name())
 }
