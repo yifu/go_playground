68e679b551e1fc5d82afb100b7878dcd0c29c280
Yves Baumes <ybaumes@gmail.com>
2018-03-22 19:08:50 +0100

    Implement opening a new file if necessary.

--- coreutils/mcp/mcp.go
+++ coreutils/mcp/mcp.go
@@ -88,24 +88,6 @@ func processAllParamAreFiles() {
 		os.Exit(1)
 	}
 
-	fileInfo1, err := os.Stat(os.Args[1])
-	if err != nil {
-		printErr(err)
-		os.Exit(2)
-	}
-
-	fileInfo2, err := os.Stat(os.Args[2])
-	if err != nil {
-		printErr(err)
-		// TODO
-		os.Exit(1)
-	}
-
-	if os.SameFile(fileInfo1, fileInfo2) {
-		os.Exit(0)
-	}
-
-	// TODO Copy the src file into the dst file open(chemin, O_WRONLY | O_TRUNC) ou open(chemin,  O_WRONLY | O_CREAT,  mode)
 	src, err := os.Open(os.Args[1])
 	if err != nil {
 		printErr(err)
@@ -113,12 +95,36 @@ func processAllParamAreFiles() {
 	}
 	defer src.Close()
 
-	dst, err := os.OpenFile(os.Args[2], os.O_WRONLY|os.O_TRUNC, fileInfo1.Mode().Perm())
+	srcStat, err := src.Stat()
 	if err != nil {
 		printErr(err)
 		os.Exit(2)
 	}
-	defer dst.Close()
+
+	dst, err := os.OpenFile(os.Args[2], os.O_WRONLY|os.O_TRUNC, 0 /*perm is useless when O_CREATE is not specified*/)
+	if err != nil {
+		if os.IsNotExist(err) {
+			dst, err := os.OpenFile(os.Args[2], os.O_WRONLY|os.O_CREATE, srcStat.Mode().Perm())
+			if err != nil {
+				printErr(err)
+				os.Exit(2)
+			}
+			defer dst.Close()
+		} else {
+			printErr(err)
+			os.Exit(1)
+		}
+	}
+
+	dstStat, err := dst.Stat()
+	if err != nil {
+		printErr(err)
+		os.Exit(2)
+	}
+
+	if os.SameFile(srcStat, dstStat) {
+		os.Exit(0)
+	}
 
 	io.Copy(dst, src)
 	os.Exit(0)
