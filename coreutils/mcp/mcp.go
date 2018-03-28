edbfe964f5915bc761e902c513670b4703b71df4
Yves Baumes <ybaumes@gmail.com>
2018-03-28 02:00:33 +0200

    Implement recursive copy.

--- coreutils/mcp/mcp.go
+++ coreutils/mcp/mcp.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // TODO Implement a test suite of some sort.
@@ -15,9 +16,11 @@ import (
 
 type pathList []string
 type environment struct {
-	oks   pathList
 	isRec bool
+	name  string
 }
+
+// TODO Make test with symlink in the middle. Maybe we should use os.Lstat() instead.
 
 func main() {
 	env := environment{}
@@ -30,40 +33,63 @@ func main() {
 	exitCode := 0
 
 	dst, srcs := parseCmdLine()
-	env.oks = make(pathList, 0, len(srcs))
+	oks := make(pathList, 0, len(srcs))
 
 	var dir string
-	if len(srcs) > 1 {
+	if len(srcs) == 1 {
+		if info, err := os.Stat(dst); err != nil {
+			if os.IsNotExist(err) {
+				dir, env.name = filepath.Split(dst)
+
+			} else {
+				printErr(err)
+				os.Exit(2)
+			}
+		} else {
+			if info.IsDir() {
+				dir = dst
+			} else {
+				dir = filepath.Dir(dst)
+			}
+		}
+	} else if len(srcs) > 1 {
 		// When len(srcs) > 1, then dst must be a dir. We check that it is the case:
 		if !checkIsDir(dst) {
 			printErr(NotADirErr{dst})
 			os.Exit(1)
 		}
+		// TODO When -r is used, we must use filepath.Split() in order to get the dir and the filename. In case of "mcp -r dir1 dir2/" then we must have dir2/dir1 as a result.
 		dir = dst
 	}
 
 	for _, src := range srcs {
-		switch {
-		case len(srcs) == 1:
-			dst = mkDst(dst, src)
-		case len(srcs) > 1:
-			_, fn := filepath.Split(src)
-			dst = filepath.Join(dir, fn)
+		dst := filepath.Join(dir, filepath.Base(src))
+		if oks.contains(dst) {
+			printErr(WillNotOverwriteErr{dst, src})
+			exitCode = 1
+			continue
 		}
+
 		var err error
-		env, err = cp(dst, src, env)
+		env, err = cp(dir, src, env)
 		if err != nil {
 			printErr(err)
 			exitCode = 1
+			continue
 		}
+
+		oks = append(oks, dst)
 	}
 	os.Exit(exitCode)
 }
 
 func parseCmdLine() (string, pathList) {
 	args := flag.Args()
-	dst := args[len(args)-1]
+	dst := filepath.Clean(args[len(args)-1])
 	srcs := args[0 : len(args)-1]
+	for i, path := range srcs {
+		srcs[i] = filepath.Clean(path)
+	}
 	return dst, srcs
 }
 
@@ -93,6 +119,7 @@ func checkIsDir(dst string) bool {
 	return err == nil && dirfi.IsDir()
 }
 
+// TODO Remove that function.
 func mkDst(dst, src string) string {
 	if dstfi, err := os.Stat(dst); err != nil {
 		if os.IsNotExist(err) {
@@ -104,7 +131,7 @@ func mkDst(dst, src string) string {
 			os.Exit(2)
 		}
 	} else if dstfi.IsDir() {
-		return filepath.Join(dst, src)
+		return filepath.Join(dst, filepath.Base(src))
 	}
 	// Nothing to do: dst is a filename, we must copy into it directly.
 	return dst
@@ -119,62 +146,124 @@ func (paths pathList) contains(path string) bool {
 	return false
 }
 
-func openFiles(dst, src string, env environment) (dstf, srcf *os.File, err error) {
-	// Open src
-	srcf, err = os.OpenFile(src, os.O_RDONLY, 0)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil, nil, NoSuchFileOrDirErr{src}
-		}
-		printErr(err)
-		os.Exit(2)
-	}
-	srcfi, err := srcf.Stat()
-	if err != nil {
-		printErr(err)
-		os.Exit(2)
-	}
-	// Various checks on src.
-	if srcfi.IsDir() {
-		return nil, nil, OmittingDirErr{src}
-	}
-	if env.oks.contains(dst) {
-		return nil, nil, WillNotOverwriteErr{dst, src}
-	}
-	// Stat(dst) first, then only opening it, to avoid creating an empty file.
-	if dstfi, err := os.Stat(dst); err != nil {
-		if !os.IsNotExist(err) {
-			printErr(err)
-			os.Exit(2)
-		}
-	} else if os.SameFile(dstfi, srcfi) {
-		// Nothing to copy.
-		return nil, nil, SameFileErr{dst, src}
-	}
-	dstf, err = os.OpenFile(dst, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, srcfi.Mode().Perm())
-	if err != nil {
-		printErr(err)
-		os.Exit(2)
-	}
-	return
+func removePrefix(s, prefix string) string {
+	return strings.TrimPrefix(s, prefix)
 }
 
-func cp(dst, src string, env environment) (environment, error) {
-	dstf, srcf, err := openFiles(dst, src, env)
+func cp(dir, src string, env environment) (environment, error) {
+	srcfi, err := os.Stat(src)
 	if err != nil {
-		switch err.(type) {
-		case SameFileErr:
-			// When src and dst are the same files, there is no copy going on.
-			// We just keep going on with the next src.
-		default:
-			return env, err
+		if os.IsNotExist(err) {
+			return env, NoSuchFileOrDirErr{src}
+		}
+		printErr(err)
+		os.Exit(2)
+	}
+
+	if srcfi.IsDir() {
+		if env.isRec {
+
+			// TODO Refactor walkFunc into its own func.
+			err := filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					printErr(err)
+					os.Exit(2)
+				}
+
+				path = filepath.Clean(path)
+
+				fmt.Printf("visited file: %q\n", path)
+				if info.IsDir() {
+					fmt.Println("path1:", path, ", src:", src)
+					// TODO Remove this function to call TrimPrefix() directly.
+					path = removePrefix(path, filepath.Dir(src))
+					fmt.Println("path2:", path)
+					if path == src && env.name != "" {
+						path = filepath.Join(filepath.Dir(path), env.name)
+						fmt.Println("path3:", path)
+					}
+
+					fmt.Println("dir=", dir, ", path=", path)
+					// Create a directory with the same name in the destination:
+					newdir := filepath.Join(dir, path)
+					err := os.Mkdir(newdir, 0700)
+					if err != nil {
+						if os.IsExist(err) {
+
+						} else {
+							return err
+						}
+					}
+				} else {
+					fmt.Println("env=", env)
+					env, err = cp(filepath.Join(dir, filepath.Dir(path)), path, environment{})
+					if err != nil {
+						return err
+					}
+					fmt.Println("env2=", env)
+				}
+
+				return nil
+			})
+
+			if err != nil {
+				printErr(err)
+				os.Exit(2)
+			}
+			// TODO Change back the permission on all the directory files after walking them.
+		} else {
+			return env, OmittingDirErr{src}
 		}
 	} else {
-		if _, err := io.Copy(dstf, srcf); err != nil {
+
+		// Open src
+		srcf, err := os.OpenFile(src, os.O_RDONLY, 0)
+		if err != nil {
 			printErr(err)
 			os.Exit(2)
 		}
-		env.oks = append(env.oks, dst)
+
+		// Stat(dir) first, then only opening it, to avoid creating an empty file.
+
+		// TODO if env.OneSrc && !dstfi.IsDir() { /*Do not touch the destination name*/ } else
+		// TODO dst = filepath.Join(dst, filepath.Base(src)) }
+
+		// TODO Rename *fi to *info
+
+		dst := ""
+		if env.name != "" {
+			dst = filepath.Join(dir, env.name)
+		} else {
+			dst = filepath.Join(dir, filepath.Base(src))
+		}
+
+		if dstfi, err := os.Stat(dst); err != nil {
+
+		} else if os.SameFile(dstfi, srcfi) {
+			// Nothing to copy.
+			return env, SameFileErr{dst, src}
+		}
+
+		dstf, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, srcfi.Mode().Perm())
+		if err != nil {
+			printErr(err)
+			os.Exit(2)
+		}
+
+		if err != nil {
+			switch err.(type) {
+			case SameFileErr:
+				// When src and dst are the same files, there is no copy going on.
+				// We just keep going on with the next src.
+			default:
+				return env, err
+			}
+		} else {
+			if _, err := io.Copy(dstf, srcf); err != nil {
+				printErr(err)
+				os.Exit(2)
+			}
+		}
 	}
 	return env, nil
 }
